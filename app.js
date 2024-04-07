f5a7c30292f4f582735906d8cfc1d0ed582c8923
Khaled Bin Monowar Hossain <111820117+khbmh@users.noreply.github.com>
2024-04-08 00:22:26 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+
+
+function show(btn, item){btn.addEventListener(`click`,()=>{
+  item.style.display = `flex`
+  // alert(`hello`)
+})}
+function hide(btn, item){btn.addEventListener(`click`,()=>{
+  item.style.display = `none`
+  // alert(`hello`)
+})}
+
+
+show(document.querySelector(`#os1cont`), document.querySelector(`#os1info`))
+hide(document.querySelector(`#os1cl`), document.querySelector(`#os1info`))
+
+show(document.querySelector(`#bp1cont`), document.querySelector(`#bp1info`))
+hide(document.querySelector(`#bp1cl`), document.querySelector(`#bp1info`))
+
+show(document.querySelector(`#qp1cont`), document.querySelector(`#qp1info`))
+hide(document.querySelector(`#qp1cl`), document.querySelector(`#qp1info`))
+
+show(document.querySelector(`#sp1cont`), document.querySelector(`#sp1info`))
+hide(document.querySelector(`#sp1cl`), document.querySelector(`#sp1info`))
+
+
+
+
+show(document.querySelector(`#os2cont`), document.querySelector(`#os2info`))
+hide(document.querySelector(`#os2cl`), document.querySelector(`#os2info`))
+
+show(document.querySelector(`#bp2cont`), document.querySelector(`#bp2info`))
+hide(document.querySelector(`#bp2cl`), document.querySelector(`#bp2info`))
+
+show(document.querySelector(`#qp2cont`), document.querySelector(`#qp2info`))
+hide(document.querySelector(`#qp2cl`), document.querySelector(`#qp2info`))
+
+show(document.querySelector(`#sp2cont`), document.querySelector(`#sp2info`))
+hide(document.querySelector(`#sp2cl`), document.querySelector(`#sp2info`))
+
+
+
+
+show(document.querySelector(`#os3cont`), document.querySelector(`#os3info`))
+hide(document.querySelector(`#os3cl`), document.querySelector(`#os3info`))
+
+show(document.querySelector(`#bp3cont`), document.querySelector(`#bp3info`))
+hide(document.querySelector(`#bp3cl`), document.querySelector(`#bp3info`))
+
+show(document.querySelector(`#qp3cont`), document.querySelector(`#qp3info`))
+hide(document.querySelector(`#qp3cl`), document.querySelector(`#qp3info`))
+
+show(document.querySelector(`#sp3cont`), document.querySelector(`#sp3info`))
+hide(document.querySelector(`#sp3cl`), document.querySelector(`#sp3info`))
+
+
+
+
+show(document.querySelector(`#os4cont`), document.querySelector(`#os4info`))
+hide(document.querySelector(`#os4cl`), document.querySelector(`#os4info`))
+
+show(document.querySelector(`#bp4cont`), document.querySelector(`#bp4info`))
+hide(document.querySelector(`#bp4cl`), document.querySelector(`#bp4info`))
+
+show(document.querySelector(`#qp4cont`), document.querySelector(`#qp4info`))
+hide(document.querySelector(`#qp4cl`), document.querySelector(`#qp4info`))
+
+show(document.querySelector(`#sp4cont`), document.querySelector(`#sp4info`))
+hide(document.querySelector(`#sp4cl`), document.querySelector(`#sp4info`))
+
+
+
+
+show(document.querySelector(`#os5cont`), document.querySelector(`#os5info`))
+hide(document.querySelector(`#os5cl`), document.querySelector(`#os5info`))
+
+show(document.querySelector(`#bp5cont`), document.querySelector(`#bp5info`))
+hide(document.querySelector(`#bp5cl`), document.querySelector(`#bp5info`))
+
+show(document.querySelector(`#qp5cont`), document.querySelector(`#qp5info`))
+hide(document.querySelector(`#qp5cl`), document.querySelector(`#qp5info`))
+
+show(document.querySelector(`#sp5cont`), document.querySelector(`#sp5info`))
+hide(document.querySelector(`#sp5cl`), document.querySelector(`#sp5info`))
+
+
+
+
+
+
+
+
+
